3d2c78b8d8ca0e875ac8a59801b5bd424daaf874
tarqab <tareq.ab.b7@gmail.com>
2024-01-05 12:20:49 +0100

    upadate

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,15 +1,67 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import toast from "react-hot-toast";
-import { auth , db} from "../../firebase.js";
+import { auth, db , storage } from "../../firebase.js";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
+
 
 export default function Register() {
   const [email, setEmail] = useState(null);
   const [firstName, setFirstName] = useState(null);
   const [lastName, setLastName] = useState(null);
   const [password, setPassword] = useState(null);
+  const [repassword, setRePassword] = useState(null);
+  const [file , setFile] = useState("")
+  const userDataObj = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    password: password,
+    repassword: repassword,
+  };
+ 
 
+  useEffect(()=>{
+  const uploadFile = () => {
+    const storageRef = ref(storage, file.name);
+    
+const uploadTask = uploadBytesResumable(storageRef, file);
+
+
+uploadTask.on('state_changed', 
+  (snapshot) => {
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case 'paused':
+        console.log('Upload is paused');
+        break;
+      case 'running':
+        console.log('Upload is running');
+        break;
+        default: 
+        break
+    }
+    
+  }, 
+  (error) => {
+    // Handle unsuccessful uploads
+  }, 
+  () => {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      console.log('File available at', downloadURL);
+    });
+  }
+);
+
+  }
+
+
+  } , [file])
   async function createNewUser(e) {
     try {
       e.preventDefault();
@@ -17,13 +69,12 @@ export default function Register() {
       const user = res.user;
       console.log(user);
       await setDoc(doc(db, "users", user.uid), {
-        firstName: firstName ,
-        lastName: lastName,
-        email:email,
-        timeStamp:serverTimestamp()
+        firstName: userDataObj.firstName,
+        lastName: userDataObj.lastName,
+        email: userDataObj.email,
+        timeStamp: serverTimestamp(),
       });
       toast.success("New account is created");
-
     } catch (error) {
       const errorMessage = error.message;
       if (errorMessage === "Firebase: Error (auth/email-already-in-use).") {
@@ -35,10 +86,10 @@ export default function Register() {
   return (
     <>
       <div className="container py-4">
-        <form className="form" onSubmit={createNewUser}> 
-        <div className="mb-3">
+        <form className="form" onSubmit={createNewUser}>
+          <div className="mb-3">
             <label htmlFor="firstName" className="form-label">
-              First name 
+              First name
             </label>
             <input
               type="text"
@@ -50,7 +101,7 @@ export default function Register() {
           </div>
           <div className="mb-3">
             <label htmlFor="lastName" className="form-label">
-              Last name 
+              Last name
             </label>
             <input
               type="text"
@@ -83,6 +134,30 @@ export default function Register() {
               placeholder="Enter your password"
               onChange={(e) => setPassword(e.target.value)}
             />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="repassword" className="form-label">
+              Repeat the Password
+            </label>
+            <input
+              type="repassword"
+              className="form-control"
+              id="repassword"
+              placeholder="Enter your password"
+              onChange={(e) => setRePassword(e.target.value)}
+            />
+          </div>
+          <div className="mb-3"  >
+            <label style={{display:"block" , marginBottom:"10px"}} htmlFor="upload">
+              Upload you profile Photo
+            </label>
+            <input id="upload"
+            type="file" 
+            onChange={(e) => setFile(e.target.files[0])}
+            >
+
+
+            </input>
           </div>
           <button className="btn btn-success" type="submit">
             Sign in
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyDTNtTaIRiUhbwWnqflm7oZlgUsJue2uu8",
@@ -15,4 +16,5 @@ export const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app) ; 
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 
